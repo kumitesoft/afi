0e03e5fb64161c91f65cac56064cda4a02f0e77c
Marczeli <aleczas1@onet.pl>
2020-08-19 13:12:13 +0200

    1

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// const withCSS = require('@zeit/next-css')
+// module.exports = withCSS({
+ 
+// })
+
+module.exports = {
+  env: {
+      MONGO: "mongodb+srv://marcin100:marcinmarcin100@afi-app.rcqkw.mongodb.net/Afi-app?retryWrites=true&w=majority"
+  },
+  // Target must be serverless
+  target: 'serverless'
+}
